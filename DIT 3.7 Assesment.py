472dce649c6597beec7a05f8b944067051b008fc
hugo-m11 <22hmawdsley@wakatipu.school.nz>
2026-04-23 12:33:11 +1200

    Add files via upload

--- DIT 3.7 Assesment.py
+++ DIT 3.7 Assesment.py
@@ -1,10 +1,21 @@
 from tkinter import*
 
+class Menu:
+    #class that makes the menu items 
+    def __init__(self, name, price, description):
+        self.name = name 
+        self.price = price
+        self.description = description
+    
+
 class SimpleGUI:
     def __init__(self, parent):
         
         self.name_var = StringVar()
+
+        menu_items = [[]]
         
+        #label to enter order name
         self.name_label = Label(parent, text="Name")
         self.name_label.pack()
         self.name_entry = Entry(parent, textvariable=self.name_var)
