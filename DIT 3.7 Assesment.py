c1191e158bc5938e9b727a1229a0bebe3576fe7a
hugo-m11 <22hmawdsley@wakatipu.school.nz>
2026-04-23 13:45:11 +1200

    Add files via upload

--- DIT 3.7 Assesment.py
+++ DIT 3.7 Assesment.py
@@ -1,29 +1,52 @@
 from tkinter import*
 
-class Menu:
+class Menu_items:
     #class that makes the menu items 
-    def __init__(self, name, price, description):
-        self.name = name 
+    def __init__(self, menu_item_name, price):
+        self.menu_item_name = menu_item_name
         self.price = price
-        self.description = description
+
+        
+        
     
 
 class SimpleGUI:
     def __init__(self, parent):
-        
+         #label to enter order name
         self.name_var = StringVar()
-
-        menu_items = [[]]
-        
-        #label to enter order name
         self.name_label = Label(parent, text="Name")
         self.name_label.pack()
         self.name_entry = Entry(parent, textvariable=self.name_var)
         self.name_entry.pack()
 
+        
+        #drop down list of food items 
+        menu_items = [Menu_items("Burger", 17),
+                      Menu_items("Cabbage and Lettuce", 18),
+                      Menu_items("Chicken Tikka Mo Salah", 22),
+                      Menu_items("50g of Rump Steak", 26),
+                      Menu_items("Pesto Pasta", 27),
+                      Menu_items("HUGE burger", 34),
+                      Menu_items("Lobster agnolotti", 43)]         
+
+        for x in menu_items:
+            print(x.menu_item_name)
+            print(x.price)
+        
+    
+
+        self.list_val = StringVar()
+        self.list_val.set(menu_items[0])
+        self.option = OptionMenu(parent, self.list_val, *menu_items)
+        self.option.pack()
+
+
 
 
 if __name__ == "__main__":
+
+
+
     root = Tk()
     gui = SimpleGUI(root)
     root.title("TAKEAWAY ORDERING SYSTEM")
