9adfd04363ca09875ba0b28134cdd1682a75be18
hugo-m11 <22hmawdsley@wakatipu.school.nz>
2026-04-25 13:23:55 +1200

    Add files via upload

--- DIT 3.7 Assesment.py
+++ DIT 3.7 Assesment.py
@@ -6,39 +6,33 @@ class Menu_items:
         self.menu_item_name = menu_item_name
         self.price = price
 
-        
-        
-    
+    def __str__(self):
+        return f"{self.menu_item_name} - ${self.price}"
 
 class SimpleGUI:
     def __init__(self, parent):
-         #label to enter order name
-        self.name_var = StringVar()
-        self.name_label = Label(parent, text="Name")
-        self.name_label.pack()
-        self.name_entry = Entry(parent, textvariable=self.name_var)
-        self.name_entry.pack()
 
-        
         #drop down list of food items 
-        menu_items = [Menu_items("Burger", 17),
+        self.menu_items = [Menu_items("Burger", 17),
                       Menu_items("Cabbage and Lettuce", 18),
                       Menu_items("Chicken Tikka Mo Salah", 22),
                       Menu_items("50g of Rump Steak", 26),
                       Menu_items("Pesto Pasta", 27),
                       Menu_items("HUGE burger", 34),
-                      Menu_items("Lobster agnolotti", 43)]         
-
-        for x in menu_items:
-            print(x.menu_item_name)
-            print(x.price)
+                      Menu_items("Lobster agnolotti", 43)]   
         
-    
+         #label to enter order name
+        self.list_val = StringVar()
+        self.list_val.set(str(self.menu_items[0]))
+        
+
+        self.option = OptionMenu(parent, self.list_val, *self.menu_items)
+        self.option.pack() 
+        
 
         self.list_val = StringVar()
-        self.list_val.set(menu_items[0])
-        self.option = OptionMenu(parent, self.list_val, *menu_items)
-        self.option.pack()
+        self.list_val.set(self.menu_items[0])
+        
 
 
 
