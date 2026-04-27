5bd6fe7fa67e6ace39e38ed8f38c1ed52cbfd18b
hugo-m11 <22hmawdsley@wakatipu.school.nz>
2026-04-28 11:37:41 +1200

    Add files via upload

--- DIT 3.7 Assesment.py
+++ DIT 3.7 Assesment.py
@@ -1,4 +1,5 @@
 from tkinter import*
+from tkinter import messagebox
 
 class Menu_items:
 #class that makes the menu items 
@@ -14,18 +15,18 @@ class SimpleGUI:
 
 #stores menu items as an attribute
         self.menu_items = [Menu_items("Burger", 17),
-                      Menu_items("Cabbage and Lettuce", 18),
-                      Menu_items("Chicken Tikka Mo Salah", 22),
-                      Menu_items("50g of Rump Steak", 26),
-                      Menu_items("Pesto Pasta", 27),
-                      Menu_items("HUGE burger", 34),
-                      Menu_items("Lobster agnolotti", 43)]   
+                           Menu_items("Cabbage and Lettuce", 18),
+                           Menu_items("Chicken Tikka Mo Salah", 22),
+                           Menu_items("50g of Rump Steak", 26),
+                           Menu_items("Pesto Pasta", 27),
+                           Menu_items("HUGE burger", 34),
+                           Menu_items("Lobster agnolotti", 43)]   
         
 #stores the varible selected from the dropdown list 
         self.list_val = StringVar()
         self.list_val.set(str(self.menu_items[0]))
 
-
+#Where the user enters their name 
         self.name_variable = StringVar()
         self.name_label = Label(parent, text="Name")
         self.name_label.pack()
@@ -37,23 +38,35 @@ class SimpleGUI:
         self.option = OptionMenu(parent, self.list_val, *self.menu_items)
         self.option.pack() 
 #button that runs the get selected item functio
-        self.button = Button(parent, text="Get Selection", command=self.get_selectet_item)
+        self.button = Button(parent, text="Add Selection to order", command=self.get_selectet_item)
         self.button.pack()
+
+   
+        
+    
 
 #checks to see if selected item is the same as the object created in the str function
     def get_selectet_item(self):
+
+        user_order = []
+        
         selected_menu_item = self.list_val.get()
+
+        user_order.append(selected_menu_item)
+
+        if self.name_entry.get() == "":
+            messagebox.showerror("", "Please add an order name")
 
 #loops through the list of menu_items to find matching object
         for item in self.menu_items:
             if str(item) == selected_menu_item:
                 print(item.menu_item_name, item.price)
-
     
+    def price_of_order():
+        pass
+
 
 if __name__ == "__main__":
-
-
 
     root = Tk()
     gui = SimpleGUI(root)
